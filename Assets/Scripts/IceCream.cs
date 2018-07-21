4dfa05946bb214c4677d319462bf63678572cee8
DESKTOP-H214NTE\james <james.veugelaers@outlook.co.nz>
2018-07-21 18:01:03 +1200

    Added Girl model and ice cream to game

---
+++ Assets/Scripts/IceCream.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCream : MonoBehaviour
+{
+    public void OnPickup()
+    {
+        // Win! :D
+    }
+}
